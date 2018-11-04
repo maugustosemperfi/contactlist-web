f5b835555a8b68c782b8ab456927b77cdad5796d
augustoprofemp@gmail.com <augustoprofemp@gmail.com>
2018-11-04 12:39:24 -0300

    [UPDATE] Updating Angular
    - Updating to Angular 7
    - Adding angular cdk to app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgxsRouterPluginModule } from '@ngxs/router-plugin';
 import { NgxsModule } from '@ngxs/store';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     NgxsModule.forRoot(),
     NgxsRouterPluginModule.forRoot(),
     BrowserModule,
+    BrowserAnimationsModule,
     AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/prime.module.ts
+++ src/app/prime.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
+import { CardModule } from 'primeng/card';
 
 @NgModule({
   imports: [
+    CardModule,
   ],
   exports: [
+    CardModule,
   ],
 })
 export class PrimeModule {}
