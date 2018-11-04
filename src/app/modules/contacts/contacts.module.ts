2ad087b81159751beb91a0862ec1eda9531a7b50
augustoprofemp@gmail.com <augustoprofemp@gmail.com>
2018-11-04 12:20:41 -0300

    [FEATURE] Adding Flex layout to application
    - Adding Flex Layout module
    - Creatig PrimeModule

--- src/app/modules/contacts/contacts.module.ts
+++ src/app/modules/contacts/contacts.module.ts
@@ -2,9 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ContactsComponent } from './contacts.component';
 import { ContactsRoutingModule } from './contacts-routing.module';
+import { PrimeModule } from '@app/prime.module';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 @NgModule({
   imports: [
+    FlexLayoutModule,
+    PrimeModule,
     ContactsRoutingModule,
     CommonModule
   ],
